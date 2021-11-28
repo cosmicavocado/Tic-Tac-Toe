5ccdfe11e2a30f2d13d60ac0d6a76a201b5e1609
Mariah Legg <mariah.legg@gmail.com>
2021-11-27 21:35:55 -0500

    can switch between 'X' and 'O'

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+const boxes = document.querySelectorAll('.box');
+let validTurns = 1;
+
+const clearBoard = () => {
+    boxes.innerText = '';
+}
 
 const listenToBoxes = () => {
-    const boxes = document.querySelectorAll('.box');
     for (let i=0; i<boxes.length; i++) {
         boxes[i].addEventListener('click', (e) => {
             e.preventDefault();
             let currentBox = e.target;
-            if (currentBox.innerText === '') {;
-                currentBox.innerText = 'X';
+            if (currentBox.innerText !== 'X' && currentBox.innerText !== 'O') {
+                if (validTurns % 2 !== 0) {
+                    currentBox.innerText = 'X';
+                } else {
+                    currentBox.innerText = 'O';
+                }
+                validTurns++;
             }
         })
     }
@@ -14,6 +24,7 @@ const listenToBoxes = () => {
 
 // Game Loop
 const runGame = () => {
+    clearBoard();
     listenToBoxes();
 }
 
