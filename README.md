2993713ee9461dd459c66732ab2d6c67ea4fab33
Mariah Legg <mariah.legg@gmail.com>
2021-11-30 16:17:27 -0500

    fixed some styling and fixed clicks allowed after game over

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This project is a simple front-end tic-tac-toe game that will allow two players on the same computer to play against eachother.
 
 ## User Stories
-* As a player...
+### As a player...
 * I should be able to start a new game
 * I should be able to click on a square to add X first and then O, and so on
 * I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-// Global Variables
 const boxes = document.querySelectorAll('.box');
-let validTurns = 0;
+let validMoves = 0;
 const gameText = document.querySelector('#currentText');
 
-// Reset Game After Delay
 const resetGame = () => {
-    for (let i=0; i<boxes.length; i++) {
-        boxes[i].innerText = '';
-    }
-    validTurns = 0;
+    boxes.forEach(box => {box.innerText = ''});
+    validMoves = 0;
     gameText.innerText = 'Player X\'s turn';
 }
 
-// Reset on Button Press
-document.querySelector('button').addEventListener('click', resetGame);
+const listenReset = () => {
+    const resetBtn = document.querySelector('button');
+    resetBtn.addEventListener('click', resetGame);
+}
 
-// Check Win, Then Check Tie
-const checkWin = () => {
-    let isTie = true;
+const isGameOver = () => {
+    let gameOver = false;
     const winConditions = [
         // horizontal
         [0, 1, 2],
@@ -39,43 +36,48 @@ const checkWin = () => {
         let matchesX = markers.filter(marker => marker === 'X' && marker !== '');
         let matchesO = markers.filter(marker => marker === 'O' && marker !== '');
         if (matchesX.length === 3 || matchesO.length === 3) {
+            gameOver = true;
             gameText.innerText = `Player ${markers[0]} wins! Resetting board...`;
-            setTimeout(() => {resetGame()}, 3000);
-            isTie = false;
+            setTimeout(() => {resetGame()}, 2500);
         }
     })
-    checkTie(isTie);
+    if (gameOver !== true) {
+        gameOver = checkTie(gameOver);
+    }
+    return gameOver;
 }
 
-// Check Tie
-const checkTie = (isTie) => {
-    if (validTurns === 9 && isTie === true) {
-        console.log('condition checked');
+const checkTie = (gameOver) => {
+    if (validMoves === boxes.length && gameOver === false) {
         gameText.innerText = 'Tie! Resetting board...';
-        setTimeout(() => {resetGame()}, 3000);
+        setTimeout(() => {resetGame()}, 2500);
+        return true;
+    } else {
+        return false;
     }
 }
 
-// Main Game Loop
+
+
 const runGame = () => {
-    for (let i=0; i<boxes.length; i++) {
-        boxes[i].addEventListener('click', (e) => {
+    let end = false;
+    boxes.forEach(box => {
+        box.addEventListener('click', (e) => {
             e.preventDefault();
             let currentBox = e.target;
-            validTurns++;
-            if (currentBox.innerText === '') {
-                if (validTurns % 2 !== 0) {
+            if (currentBox.innerText === '' && end === false) {
+                validMoves++;
+                if (validMoves % 2 !== 0) {
                     currentBox.innerText = 'X';
                     gameText.innerText = 'Player O\'s turn';
                 } else {
                     currentBox.innerText = 'O';
                     gameText.innerText = 'Player X\'s turn';
                 }
-                checkWin();
+                end = isGameOver();
             }
         })
-    }
+    })
 }
 
-// Run Game
 runGame();
