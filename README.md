c60f516a09cd4c77a4f9b47d0c485ea5225a910a
Mariah Legg <mariah.legg@gmail.com>
2021-12-01 09:46:38 -0500

    reduce used instead of filter in win condtions, README added, wireframe image added

--- README.md
+++ README.md
@@ -1,12 +1,101 @@
 # Tic-Tac-Toe
-This project is a simple front-end tic-tac-toe game that will allow two players on the same computer to play against eachother.
+
+This project is a simple front-end tic-tac-toe game that allows two players on the same computer to play against eachother.
+
+## Technologies & Resources
+
+* Version Control
+    * Ubuntu Subsystem For Windows: Used Ubuntu based Linux subsystem for Windows to directly interact with files
+    * Git: version control
+    * GitHub: remote repository and project deployment
+* Development
+    * Google Chrome Browser: displaying game
+    * Google Chrome Developer Tools: debugging and troubleshooting code
+    * VS Code: used to create index, script, styles, and README
+        * [markdownlint](https://marketplace.visualstudio.com/items?itemName=DavidAnson.vscode-markdownlint): VS Code extension used to assist with README formatting
+* Design
+    * [Google Fonts](https://fonts.google.com/):
+    * Press Start 2P (h1)
+    * Open Sans (body)
+    * [Color Palette](https://www.color-hex.com/color-palette/97670): used color-hex.com for color scheme
+    * [Wireframe](https://wireframe.cc): used wireframe.cc to design the UI
+
+# Design Overview
+
+## Wireframe
+
+[Wireframe Link](https://wireframe.cc/AiUf3P)
+
+![Wireframe Image](tic-tac-toe-wireframe.PNG)
 
 ## User Stories
-### As a player...
-* I should be able to start a new game
-* I should be able to click on a square to add X first and then O, and so on
-* I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
-* I should not be able to click the same square twice
-* I should be shown a message when I win, lose or tie
-* I should not be able to continue playing once I win, lose, or tie
-* I should be able to play the game again without refreshing the page
+
+* As a user, I should be able to start a new tic tac toe game
+* As a user, I should be able to click on a square to add X first and then O, and so on
+* As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
+* As a user, I should not be able to click the same square twice
+* As a user, I should be shown a message when I win, lose or tie
+* As a user, I should not be able to continue playing once I win, lose, or tie
+* As a user, I should be able to play the game again without refreshing the page
+
+# Demo Outline
+
+* Shown current player's turn message
+* Can't click same square twice
+* Solve for winner
+    * Shown message on win
+* Check for tie
+    * Shown message on tie
+* Won't allow user to keep clicking after game over (auto reset 2.5 seconds)
+* User can reset game if they like
+    * Except if game is already over because of auto wipe
+
+# Approach
+
+## How I planned for the project
+
+* Wireframe
+* Added bare bones necessities for HTML and CSS
+* Psuedocode for script
+* Script
+* README
+
+## How I solved for the winner
+
+* Created a 2D array where each inner array holds the indices of a possible win condition
+* For each win condition and each index I looped the grid boxes pushing the box contents into a temporary array
+    * Reduced markers array to count the instances of each possible result ('X', 'O', '')
+* If either filtered array has length === 3
+    * Game end is true
+* If game has not ended
+    * Check for tie
+* Return boolean result to trigger end of game or continue
+
+## Hurdles
+
+* Code Structure
+    * Which code should be contained within its own function
+    * Which variables should be global
+    * Generally keeping it neat
+* Wanted to optimize code more than I did even though time was not abundant
+* Spent a lot of time researching documentation and markup
+* Struggled with breaking down my processes for the project in a way that other people can understand
+    * Habit of mixing code into pseudocode
+
+# Takeaways
+
+## What I would do differently
+
+* Make a message text function so that gameText.innerText in my code isn't as repetitive
+* Add a separate function for tracking player turns instead of having that built into the main game loop
+* Use a 2D array to track and update the board state every click
+
+## What I learned
+
+* Markdown formatting!
+* (Re)learned how to reduce arrays to count objects thanks to Usman's vote count example and MDN docs
+
+## What would I do next
+
+* Add sound effects
+* Add ability for players to choose their markers and substitute their choice for X and Y respectively
--- script.js
+++ script.js
@@ -1,22 +1,73 @@
-const boxes = document.querySelectorAll('.box');
-let validMoves = 0;
-let endGame = false;
-const gameText = document.querySelector('#currentText');
+/**
+ * # GLOBAL VARIABLES
+ * - boxes: node list of grid spaces
+ * - validMoves: used to read/update game board and check for tie
+ * - end: boolean used to signal end of game
+ * - gameText: DOM target to read/update game text
+ */
+ const boxes = document.querySelectorAll('.box');
+ let validMoves = 0;
+ let endGame = false;
+ const gameText = document.querySelector('#currentText');
+ 
 
-const resetGame = () => {
-    boxes.forEach(box => {box.innerText = '';})
-    endGame = false;
-    validMoves = 0;
-    gameText.innerText = 'Player X\'s turn';
-}
-
-const listenReset = () => {
-    const resetBtn = document.querySelector('button');
-    resetBtn.addEventListener('click', resetGame);
-}
-
+ /**
+  * # PURPOSE
+  * Clears boxes, resets endGame boolean to false, resets gameText
+  * 
+  * # LOGIC
+  * FOREACH box in boxes
+  *     clear text in boxes
+  *     reset endGame flag to false
+  *     gameText reset
+  */
+ const resetGame = () => {
+     boxes.forEach(box => {box.innerText = '';})
+     endGame = false;
+     validMoves = 0;
+     gameText.innerText = 'Player X\'s turn';
+ }
+ 
+ /**
+  * # PURPOSE
+  * Run resetGame() when the user clicks resetBtn
+  * 
+  * # LOGIC
+  * select reset button from DOM
+  * listen for clicks on reset button
+  * IF game is not over
+  *     reset game on click
+  */
+ const listenReset = () => {
+     const resetBtn = document.querySelector('button');
+     resetBtn.addEventListener('click', () => {
+         if (endGame === false) {
+             resetGame();
+         }
+    })
+ }
+ 
+ /**
+  * # PURPOSE
+  * Returns boolean endGame to stop or continue the game
+  * 
+  * # LOGIC
+  * FOREACH condition in winCondtions
+  *     Create empty array (markers) to store results
+  *     FOREACH index within conditions
+  *         Push marker into markers array
+  *     Reduce marker array to get count of idividual markers
+  *     IF reduced array 'X' === 3 or reduced array 'O' === 3
+  *         flag endGame as true
+  *         update game text
+  *         reset game after timeout
+  *     IF game didn't end in win
+  *         check for tie
+  *     endGame result is true or false
+  *         
+  * @returns endGame (boolean): used to trigger end of game
+  */
 const isGameOver = () => {
-    let gameOver = false;
     const winConditions = [
         // horizontal
         [0, 1, 2],
@@ -35,49 +86,88 @@ const isGameOver = () => {
         condition.forEach(index => {
             markers.push(boxes[index].innerText);   
         })
-        let matchesX = markers.filter(marker => marker === 'X' && marker !== '');
-        let matchesO = markers.filter(marker => marker === 'O' && marker !== '');
-        if (matchesX.length === 3 || matchesO.length === 3) {
-            gameOver = true;
+        let matches = markers.reduce((allMarkers, marker) => {
+            if (marker in allMarkers) {
+                allMarkers[marker]++;
+            } else {
+                allMarkers[marker] = 1;
+            }
+            return allMarkers;
+        }, [])
+        if (matches['X'] === winConditions[0].length || matches['O'] === winConditions[0].length) {
+            endGame = true;
             gameText.innerText = `Player ${markers[0]} wins! Resetting board...`;
             setTimeout(() => {resetGame()}, 2500);
         }
     })
-    if (gameOver !== true) {
-        gameOver = checkTie(gameOver);
+    if (endGame !== true) {
+        endGame = checkTie();
     }
-    return gameOver;
+    return endGame;
 }
 
-const checkTie = (gameOver) => {
-    if (validMoves === boxes.length && gameOver === false) {
-        gameText.innerText = 'Tie! Resetting board...';
-        setTimeout(() => {resetGame()}, 2500);
-        return true;
-    } else {
-        return false;
-    }
-}
-
-const runGame = () => {
-    listenReset();
-    boxes.forEach(box => {
-        box.addEventListener('click', (e) => {
-            e.preventDefault();
-            let currentBox = e.target;
-            if (currentBox.innerText === '' && endGame === false) {
-                validMoves++;
-                if (validMoves % 2 !== 0) {
-                    currentBox.innerText = 'X';
-                    gameText.innerText = 'Player O\'s turn';
-                } else {
-                    currentBox.innerText = 'O';
-                    gameText.innerText = 'Player X\'s turn';
-                }
-                endGame = isGameOver();
-            }
-        })
-    })
-}
-
-runGame();
+ /**
+  * # PURPOSE
+  * Takes boolean gameOver and returns true or false
+  * 
+  * # LOGIC
+  * IF valid moves are expended & the game has not been won
+  *     display tie message
+  *     reset game after 2.5 seconds
+  *     result is true
+  * ELSE tie
+  *     result is false
+  * 
+  * @param {boolean} endGame
+  * @returns boolean value (true or false)
+  */
+ const checkTie = () => {
+     if (validMoves === boxes.length) {
+         gameText.innerText = 'Tie! Resetting board...';
+         setTimeout(() => {resetGame()}, 2500);
+         return true;
+     } else {
+         return false;
+     }
+ }
+ 
+ /**
+  * # PURPOSE
+  * Contains game logic
+  * 
+  * ## LOGIC
+  * listen for reset button press
+  * FOREACH box listen for click
+  *     prevent page reload
+  *     current box is target box
+  *     IF current box is a valid target and game is not over
+  *     increment valid moves
+  *        IF valid moves is odd
+  *             current box text is 'X'
+  *             update game text for player O's turn
+  *         ELSE
+  *             current box text is 'O'
+  *             update game text for player X's turn
+  *         check for end of game
+  */
+ const runGame = () => {
+     listenReset();
+     boxes.forEach(box => {
+         box.addEventListener('click', (e) => {
+             e.preventDefault();
+             let currentBox = e.target;
+             if (currentBox.innerText === '' && endGame === false) {
+                 validMoves++;
+                 if (validMoves % 2 !== 0) {
+                     currentBox.innerText = 'X';
+                     gameText.innerText = 'Player O\'s turn';
+                 } else {
+                     currentBox.innerText = 'O';
+                     gameText.innerText = 'Player X\'s turn';
+                 }
+                 endGame = isGameOver();
+             }
+         })
+     })
+ }
+ runGame();
